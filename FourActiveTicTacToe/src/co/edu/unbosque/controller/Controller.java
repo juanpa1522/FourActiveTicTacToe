f80898240143ae48b0c7254d26853814f14d8d8b
juanpa1522 <jaraquemera@gmail.com>
2020-03-24 20:52:56 -0500

    Version 3.0 corrección de lectura desde los JTextField

--- FourActiveTicTacToe/src/co/edu/unbosque/controller/Controller.java
+++ FourActiveTicTacToe/src/co/edu/unbosque/controller/Controller.java
@@ -14,21 +14,40 @@ public class Controller implements ActionListener{
 	public Controller() {
 		view=new View();
 		view.getBotonJuego().addActionListener(this);
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource().equals(view.getBotonJuego())) {
-			for(int i=0;i<3;i++) {
-				for(int j=0;j<3;j++) {
-					view.getPanelTablero().getTablero()[i][j].getText();
-					System.out.println(view.getPanelTablero().getTablero()[i][j].getText());
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				//view.getPanelTablero().getTablero().addActionListener(this);
 				}
-				}
-			//comp=new Computadora(view.getPanelTablero().getTablero());
-			//view.getPanelTablero().reescribirTablero(comp.escribirRespuesta());
-			
+			}
 		}
+	public void actionPerformed(ActionEvent e) {
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.println(((entregarTablero()[i][j])));	
+			}
+		}
+		comp=new Computadora(entregarTablero());
+		view.getPanelTablero1().reescribirTablero(comp.escribirRespuesta());
 		
 	}
-
+	public String[][] entregarTablero(){
+		String matriz[][]= new String [3][3];
+		while(!view.getPanelTablero1().getCampo1().getText().equalsIgnoreCase("x") && !view.getPanelTablero1().getCampo1().getText().equalsIgnoreCase("")){
+			JOptionPane.showMessageDialog(null, "Por favor escriba solo X");
+			view.getPanelTablero1().getCampo1().setText("");
+		}
+		matriz[0][0]=view.getPanelTablero1().getCampo1().getText().toUpperCase();
+		matriz[0][1]=view.getPanelTablero1().getCampo2().getText().toUpperCase();
+		matriz[0][2]=view.getPanelTablero1().getCampo3().getText().toUpperCase();
+		matriz[1][0]=view.getPanelTablero1().getCampo4().getText().toUpperCase();
+		matriz[1][1]=view.getPanelTablero1().getCampo5().getText().toUpperCase();
+		matriz[1][2]=view.getPanelTablero1().getCampo6().getText().toUpperCase();
+		matriz[2][0]=view.getPanelTablero1().getCampo7().getText().toUpperCase();
+		matriz[2][1]=view.getPanelTablero1().getCampo8().getText().toUpperCase();
+		matriz[2][2]=view.getPanelTablero1().getCampo9().getText().toUpperCase();
+		return matriz;
+	}
+					
 }
+
+
--- FourActiveTicTacToe/src/co/edu/unbosque/model/Computadora.java
+++ FourActiveTicTacToe/src/co/edu/unbosque/model/Computadora.java
@@ -1,22 +1,12 @@
 package co.edu.unbosque.model;
 
-import javax.swing.JTextField;
 
 public class Computadora {
 	private String[][] tablero= new String[3][3];
 	
-	public Computadora(JTextField tablero[][]) {
-		this.tablero= obtenerTablero(tablero);
+	public Computadora(String tablero[][]) {
+		this.tablero=tablero;
 		
-	}
-	public String[][] obtenerTablero(JTextField tablero[][]){
-		String matriz[][]= new String [3][3];
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
-				matriz[i][j]=tablero[i][j].getText();
-			}
-		}
-		return matriz;
 	}
 	public String[][] escribirRespuesta() {
 		String matriz[][]= getTablero();
